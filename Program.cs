1cefb167212ebc3e7f48b39d7da49ab242ab750f
Tatiana <kravchenkodevelop@gmail.com>
2024-08-30 19:02:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PasswordGen;
+
+Console.WriteLine("get password length:");
+var passLength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("random char pass: " + Randomiser.GetPassworRndCharacters(passLength));
+Console.ReadKey();
+
